912663f4e258a7168da89e2b66f6d37b3ca7a9ab
DESKTOP-MUQBT8I\Vinni <volod.vinichuk@gmail.com>
2018-03-10 00:15:01 +0200

    #178 signed on header

--- src/components/layout/SignedOnHeader.js
+++ src/components/layout/SignedOnHeader.js
@@ -1,11 +1,66 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Navbar, Nav, NavItem, Col, Row } from 'react-bootstrap';
 
-export default class SignedOnHeader extends Component {
+import routes from '../../constants/Routes';
+import roles from '../../constants/Roles';
+import authStorage from '../../storage/AuthStorage';
+
+class SignedOnHeader extends Component {
+
+    onLink = (route) => {
+        this.props.history.push(route)
+    };
+
+    getCaptainLinks = () => {
+        return [
+            { route: routes.register, title: 'Register' },
+            { route: routes.invites, title: 'Invites' },
+            { route: routes.tasksList, title: 'Tasks' },
+            { route: routes.voterList, title: 'Voters' },
+            { route: routes.community, title: 'Community' },
+            { route: routes.captainsDashboard, title: 'Dashboard' }
+        ]
+    };
+
+    getAdminLinks = () => {
+        return [
+            { route: routes.register, title: 'Register' },
+            { route: routes.invites, title: 'Invites' },
+            { route: routes.community, title: 'Community' },
+            { route: routes.adminDashboard, title: 'Dashboard' }
+        ]
+    };
+
+    resolveLinks = () => {
+        return authStorage.getCurrentRole() === roles.captain
+            ? this.getCaptainLinks()
+            : this.getAdminLinks();
+    };
+
     render() {
         return (
             <div className="btw-on-header">
-                auth header
+                <Row>
+                    <Col>
+
+                    </Col>
+                </Row>
+                <Navbar>
+                    <Nav>
+                        { this.resolveLinks().map((link, i) => {
+                                return (
+                                    <NavItem eventKey={i} onClick={() => this.onLink(link.route)} >
+                                        { link.title }
+                                    </NavItem>
+                                );
+                            })
+                        }
+                    </Nav>
+                </Navbar>
             </div>
         )
     }
 }
+
+export default withRouter(SignedOnHeader)
