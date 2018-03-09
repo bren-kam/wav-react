0452ffb9492e0da3fe9c0823cb2fb6a5ad1107e5
DESKTOP-MUQBT8I\Vinni <volod.vinichuk@gmail.com>
2018-03-10 01:15:50 +0200

    #178 signed on header

--- src/components/layout/SignedOnHeader.js
+++ src/components/layout/SignedOnHeader.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Navbar, Nav, NavItem, Col, Row } from 'react-bootstrap';
+import {
+    Navbar,
+    Nav,
+    NavItem,
+    Col,
+    Row,
+    NavDropdown,
+    MenuItem
+} from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 
 import routes from '../../constants/Routes';
 import roles from '../../constants/Roles';
@@ -40,17 +49,24 @@ class SignedOnHeader extends Component {
 
     render() {
         return (
-            <div className="btw-on-header">
+            <div className='btw-on-header'>
                 <Row>
-                    <Col>
-
+                    <Col md={2} mdOffset={10} className='btw-nav-dropdown'>
+                        <FontAwesome className='btw-avatar'
+                                     name='user-circle'
+                                     size='3x' />
+                        <NavDropdown eventKey={1} title="" id="nav-dropdown">
+                            <MenuItem eventKey={1.1}>Profile</MenuItem>
+                            <MenuItem eventKey={1.2}>Settings</MenuItem>
+                            <MenuItem eventKey={1.3}>Sign out</MenuItem>
+                        </NavDropdown>
                     </Col>
                 </Row>
                 <Navbar>
                     <Nav>
                         { this.resolveLinks().map((link, i) => {
                                 return (
-                                    <NavItem eventKey={i} onClick={() => this.onLink(link.route)} >
+                                    <NavItem key={i} eventKey={i} onClick={() => this.onLink(link.route)} >
                                         { link.title }
                                     </NavItem>
                                 );
