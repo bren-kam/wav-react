7d1e84bae7936a543aceda3dda87b730efb7f82d
DESKTOP-MUQBT8I\Vinni <volod.vinichuk@gmail.com>
2018-03-17 03:11:47 +0200

    #245 Dashboard Markup design and implementation

--- src/components/captainsDashboard/CaptainsDashboard.js
+++ src/components/captainsDashboard/CaptainsDashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Row, Col } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
 import BaseComponent from '../../components/shared/BaseComponent';
@@ -14,59 +15,68 @@ class CaptainsDashboard extends BaseComponent {
     render() {
         const { profile: { data, isSuccess } } = this.props;
         const votersCount = 27,
-              level = 3,
+              invitesCount = 20,
               notificationCount = 5;
 
         return (
             <div className='container btw-captains-dashboard'>
                 <NavImage />
                 { isSuccess &&
-                    <div>
-                        <div className='name'>
-                            { data.firstname } { data.lastname }
-                        </div>
-                        <div className='flex-center'>
-                            <div className='blue-circle tasks flex-center'
-                                 onClick={() => this.onLink(routes.tasksList)}>
-                                <div className='notification'>{notificationCount}</div>
-                                <div>
-                                    <div className='tasks-label'>Tasks</div>
-                                    <FontAwesome name='tasks'
-                                                 size='4x'/>
+                    <Row>
+                        <Col md={8}>
+                            <Row>
+                                <Col md={6}>
+                                    <div className='icon-div tasks'>
+                                        <FontAwesome name='tasks' size='3x'/>
+                                        <span className='button-text'>Your Tasks</span>
+                                    </div>
+                                </Col>
+                                <Col md={6}>
+                                    <div className='icon-div voters'>
+                                        <FontAwesome name='thumbs-up' size='3x'/>
+                                        <span className='button-text'>
+                                            Voters <span>
+                                                (<b>{votersCount}</b>)
+                                            </span>
+                                        </span>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md={6}>
+                                    <div className='icon-div invites'>
+                                        <FontAwesome name='envelope-open'  size='3x' />
+                                        <span className='button-text'>
+                                            Invites <span>
+                                                (<b>{invitesCount}</b>)
+                                            </span>
+                                        </span>
+                                    </div>
+                                </Col>
+                                <Col md={6}>
+                                    <div className='icon-div forum'>
+                                        <FontAwesome name='comments' size='3x'/>
+                                        <span className='button-text'>Forum</span>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col md={4}>
+                            <div className='right-column'>
+                                <div className='notification'>
+                                    <FontAwesome name='bell'  size='2x'/>
+                                    <span className='label'>Notifications</span>
+                                    <span className='count'>
+                                      {notificationCount}
+                                    </span>
+                                </div>
+                                <div className='community'>
+                                    <FontAwesome name='users' size='2x' />
+                                    <span className='label'>Community</span>
                                 </div>
                             </div>
-                        </div>
-                        <div className='flex-center'>
-                            <div className='icons-container'>
-                                <div onClick={() => this.onLink(routes.voterList)}>
-                                    <div className='blue-circle link-icon flex-center'>
-                                        <div>
-                                            <FontAwesome name='file' size='3x' />
-                                            <div className='green-text'>{ votersCount }</div>
-                                        </div>
-                                    </div>
-                                    <span className='blue-label'>Voters</span>
-                                </div>
-                                <div onClick={() => this.onLink(routes.community)}>
-                                    <div className='blue-circle link-icon flex-center'>
-                                        <FontAwesome className='green-text' name='users' size='3x' />
-                                    </div>
-                                    <span className='blue-label'>Community</span>
-                                </div>
-                                <div className='link-icon'>
-                                    <div className='grey-circle link-icon flex-center'>
-                                        <FontAwesome className='blue-text' name='trophy' size='4x' />
-                                        <div className='level'>{level}</div>
-                                        <div className='arc'></div>
-                                    </div>
-                                    <span className='blue-label'> Level {level}</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='dynamic-content flex-center'>
-                            Dynamic content here
-                        </div>
-                    </div>
+                        </Col>
+                    </Row>
                 }
             </div>
         )
