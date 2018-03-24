f47e60a9a19ce2cc3d9049f16724206ba3012ff5
DESKTOP-MUQBT8I\Vinni <volod.vinichuk@gmail.com>
2018-03-25 00:04:12 +0200

    #265 unit tests for possible match page

--- src/tests/action/VoterAction.test.js
+++ src/tests/action/VoterAction.test.js
@@ -1,17 +1,24 @@
-import { expect } from 'chai'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import { makeListPersist } from "../../actions/VoterAction";
-import VoterContants from "../../constants/VoterConstants";
+import { expect } from 'chai';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import {
+	makeListPersist,
+    voterDetailsPersist,
+    matchListPersist,
+    nextNumberPersist,
+    resetVoterState
+} from '../../actions/VoterAction';
+import VoterContants from '../../constants/VoterConstants';
+import InitialState from "../../constants/InitialState";
 
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-/* Test Makelist */
-describe('btwMakelist', () => {
+/* Voter actions tests */
+describe('makelist', () => {
 
-	/* Persist Test */
 	describe('Persist makelist', () => {
 
 		const makelist = {
@@ -23,9 +30,9 @@ describe('btwMakelist', () => {
 			"lastname3" 	: "lastname3",
 			"firstname4"    : "firstname4",
 			"lastname4"		: "lastname4",
-		}
+		};
 
-		it('it should dispatch a success', () => {
+		it('it should dispatch two actions', () => {
 
 			const expectedActions = [
 				{
@@ -36,10 +43,154 @@ describe('btwMakelist', () => {
 					makeList: makelist
 				},
 			];
-			const store = mockStore({response : []});
+			const store = mockStore(InitialState);
 
 			store.dispatch( makeListPersist( makelist ) );
             expect(store.getActions()).to.deep.equal(expectedActions);
 		})
 	})
-})
+});
+
+describe('voterDetails', () => {
+
+    describe('Persist makelist', () => {
+
+        const voterDetails = {
+            address: "Morozna st. 21",
+            birthday: "1988",
+            city: "New York",
+            email: "volod.vinichuk@gmail.com",
+            gender: "Male",
+            phone: "",
+            state: "CA",
+            zip: "79066"
+        };
+
+        it('it should dispatch action', () => {
+
+            const expectedActions = [
+                {
+                    type: VoterContants.VOTER_DETAILS_PERSIST,
+                    voterDetails: voterDetails
+                },
+            ];
+            const store = mockStore(InitialState);
+
+            store.dispatch( voterDetailsPersist( voterDetails ) );
+            expect(store.getActions()).to.deep.equal(expectedActions);
+        })
+    })
+});
+
+describe('matchList', () => {
+
+    describe('Persist matchList', () => {
+
+        const voterDetails = {
+            address: "Morozna st. 21",
+            birthday: "1988",
+            city: "New York",
+            email: "volod.vinichuk@gmail.com",
+            gender: "Male",
+            phone: "",
+            state: "CA",
+            zip: "79066"
+        };
+
+        const matchList = [
+            {
+                "matchRate": 0.8076923,
+                "firstname": "DIANE1",
+                "lastname": "TURNER1",
+                "regaddrline1": "922 E ADAMS BLVD",
+                "regaddrline2": "APT 9",
+                "regaddrcity": "LOS ANGELES",
+                "regaddrstate": "CA",
+                "voterstatus": "active"
+            },
+            {
+                "matchRate": 0.9,
+                "firstname": "DIANE2",
+                "lastname": "TURNER2",
+                "regaddrline1": "922 E ADAMS BLVD",
+                "regaddrline2": "APT 9",
+                "regaddrcity": "LOS ANGELES",
+                "regaddrstate": "CA",
+                "voterstatus": "active"
+            },
+            {
+                "matchRate": 0.89,
+                "firstname": "DIANE3",
+                "lastname": "TURNER3",
+                "regaddrline1": "922 E ADAMS BLVD",
+                "regaddrline2": "APT 9",
+                "regaddrcity": "LOS ANGELES",
+                "regaddrstate": "CA",
+                "voterstatus": "offline"
+            },
+            {
+                "matchRate": 0.81,
+                "firstname": "DIANE4",
+                "lastname": "TURNER4",
+                "regaddrline1": "922 E ADAMS BLVD",
+                "regaddrline2": "APT 9",
+                "regaddrcity": "LOS ANGELES",
+                "regaddrstate": "CA",
+                "voterstatus": "active"
+            }
+        ];
+
+        it('it should dispatch two actions', () => {
+
+            const expectedActions = [
+				{
+					type: VoterContants.VOTER_MATCHLIST_PERSIST,
+                    matchList: []
+				},
+                {
+                    type: VoterContants.VOTER_MATCHLIST_PERSIST,
+                    matchList: matchList
+                },
+            ];
+            const store = mockStore(InitialState);
+
+            store.dispatch( matchListPersist( voterDetails ));
+            expect(store.getActions()).to.deep.equal(expectedActions);
+        })
+    })
+});
+
+describe('nextNumber', () => {
+    describe('nextNumber test', () => {
+        it('it should dispatch action', () => {
+
+            const expectedActions = [
+                {
+                    type: VoterContants.VOTER_NEXT_MUMBER_PERSIST,
+                },
+            ];
+            const store = mockStore(InitialState);
+
+            store.dispatch( nextNumberPersist() );
+            expect(store.getActions()).to.deep.equal(expectedActions);
+        })
+    })
+});
+
+describe('resetVoterState', () => {
+
+    describe('reset voter State ', () => {
+        it('it should dispatch action', () => {
+
+            const expectedActions = [
+                {
+                    type: VoterContants.VOTER_RESET_STATE,
+                },
+            ];
+            const store = mockStore(InitialState);
+
+            store.dispatch( resetVoterState() );
+            expect(store.getActions()).to.deep.equal(expectedActions);
+        })
+    })
+});
--- src/tests/reducers/VoterReducer.test.js
+++ src/tests/reducers/VoterReducer.test.js
@@ -1,8 +1,8 @@
 import isEqual from 'lodash/isequal'
-import {expect} from 'chai';
-import { btwMakelist } from "../../reducers/VoterReducer";
-import VoterContants from "../../constants/VoterConstants";
-
+import { expect } from 'chai';
+import voterReducer from '../../reducers/VoterReducer';
+import VoterContants from '../../constants/VoterConstants';
+import InitialState from '../../constants/InitialState';
 
 const makelist = {
 	"firstname1" 	: "firstname1",
@@ -13,20 +13,103 @@ const makelist = {
 	"lastname3" 	: "lastname3",
 	"firstname4"    : "firstname4",
 	"lastname4"		: "lastname4",
-}
+};
 
-describe('btwMakelist', () => {
+const voterDetails = {
+    address: "Morozna st. 21",
+    birthday: "1988",
+    city: "New York",
+    email: "volod.vinichuk@gmail.com",
+    gender: "Male",
+    phone: "",
+    state: "CA",
+    zip: "79066"
+};
 
-	it('should return the btwMakelist initial state', () => {
-		expect(isEqual(btwMakelist({}, {}), {})).to.be.true
+const matchList = [
+    {
+        "matchRate": 0.8076923,
+        "firstname": "DIANE1",
+        "lastname": "TURNER1",
+        "regaddrline1": "922 E ADAMS BLVD",
+        "regaddrline2": "APT 9",
+        "regaddrcity": "LOS ANGELES",
+        "regaddrstate": "CA",
+        "voterstatus": "active"
+    },
+    {
+        "matchRate": 0.9,
+        "firstname": "DIANE2",
+        "lastname": "TURNER2",
+        "regaddrline1": "922 E ADAMS BLVD",
+        "regaddrline2": "APT 9",
+        "regaddrcity": "LOS ANGELES",
+        "regaddrstate": "CA",
+        "voterstatus": "active"
+    },
+    {
+        "matchRate": 0.89,
+        "firstname": "DIANE3",
+        "lastname": "TURNER3",
+        "regaddrline1": "922 E ADAMS BLVD",
+        "regaddrline2": "APT 9",
+        "regaddrcity": "LOS ANGELES",
+        "regaddrstate": "CA",
+        "voterstatus": "offline"
+    },
+    {
+        "matchRate": 0.81,
+        "firstname": "DIANE4",
+        "lastname": "TURNER4",
+        "regaddrline1": "922 E ADAMS BLVD",
+        "regaddrline2": "APT 9",
+        "regaddrcity": "LOS ANGELES",
+        "regaddrstate": "CA",
+        "voterstatus": "active"
+    }
+];
+
+describe('initialState', () => {
+    it('should return the voter initial state', () => {
+        expect(isEqual(voterReducer(InitialState.voter, {}), InitialState.voter)).to.be.true
+    });
+});
+
+describe('makeList', () => {
+	it('should return the makeList persist', () => {
+		expect(isEqual(voterReducer(InitialState.voter, {
+			type: VoterContants.VOTER_MAKELIST_PERSIST,
+			makeList: makelist
+			}), {... InitialState.voter, ... { makeList: makelist }}
+		)).to.be.true
 	});
+});
 
-	it('should return the btwMakelist persist', () => {
-		expect(isEqual(btwMakelist({}, {
-			type           : VoterContants.VOTER_MAKELIST_PERSIST,
-			makelist	   : makelist
-		}), {
-			makelist       : makelist
-		})).to.be.true
-	});
-})
+describe('nextNumber', () => {
+    it('should return the next voter number persist', () => {
+        expect(isEqual(voterReducer(InitialState.voter, {
+                type: VoterContants.VOTER_NEXT_MUMBER_PERSIST,
+            }), {... InitialState.voter, ... { currentNumber: 2 }}
+        )).to.be.true
+    });
+});
+
+describe('voterDetails', () => {
+    it('should return voter details persist', () => {
+        expect(isEqual(voterReducer(InitialState.voter, {
+                type: VoterContants.VOTER_DETAILS_PERSIST,
+                voterDetails
+            }), {... InitialState.voter, ... { voterDetails: { 1: voterDetails } }}
+        )).to.be.true
+    });
+});
+
+describe('matchList', () => {
+    it('should return match list persist', () => {
+        expect(isEqual(voterReducer(InitialState.voter, {
+                type: VoterContants.VOTER_MATCHLIST_PERSIST,
+                matchList
+            }), {... InitialState.voter, ... { matchList }}
+        )).to.be.true
+    });
+});
