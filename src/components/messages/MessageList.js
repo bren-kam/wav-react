4574af0ee5aabb1d04ea60c0b5ecb16a3bc3ffa9
Kenneth Obikwelu <kenneth.obikwelu@kp.org>
2018-04-22 18:25:12 -0700

    adding hotfix 2 for messages

--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -11,77 +11,78 @@ import ChatBody from './ChatBody';
 import { loadChats, selectChat } from "../../actions/MessagesAction";
 
 class MessageList extends BaseComponent {
-    componentWillMount() {
-        const { actions, chats: { isSuccess, error } } = this.props;
-        if (!isSuccess && !error) {
-            actions.loadChats();
-        }
-    }
+	componentWillMount() {
+		this.props.actions.loadChats();
+	}
 
-    formatMessage = (msg) => {
-        const jsonMsg = msg.parseJson();
-        if (jsonMsg) {
-            return (
-                <div>
-                    <div>Task: { jsonMsg.task }</div>
-                    <div>Question: { this.cutMsg(jsonMsg.question )}</div>
-                </div>
-            )
-        }
-        return this.cutMsg(msg);
-    };
+	formatMessage = (msg) => {
+		const jsonMsg = msg.parseJson();
+		if (jsonMsg) {
+			return (
+				<div>
+					<div>Task: { jsonMsg.task }</div>
+					<div>Question: { this.cutMsg(jsonMsg.question )}</div>
+				</div>
+			)
+		}
+		return this.cutMsg(msg);
+	};
 
-    cutMsg = (msg) => {
-        return `${msg.substring(0,100)}...`;
-    };
+	cutMsg = (msg) => {
+		return `${msg.substring(0,100)}...`;
+	};
 
 
-    render() {
-        const { chats, selectedChatId } = this.props.chats;
+	render() {
+		const { chats, selectedChatId } = this.props.chats;
 
-        return (
-            <div className='container btw-message-list'>
-                <Typography>Message list</Typography>
-                <Row className='chat-content'>
-                    <Col md={4}>
-                        <div className='chats'>
-                            { chats.map((chat, i) => {
-                                return (
-                                    <Row key={i}
-                                         className={classnames('chat', { 'selected': chat._id === selectedChatId })}
-                                         onClick={() => this.props.actions.selectChat(chat._id) }>
-                                        <Col md={8}>
-                                            <div>{ this.formatMessage(chat.message) }</div>
-                                        </Col>
-                                        <Col md={4}>
-                                            <div>
-                                                <Moment format="MM-DD HH:mm">
-                                                    { chat.date }
-                                                </Moment>
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                )
-                            })}
-                        </div>
-                    </Col>
-                    <Col md={8}>
-                        <ChatBody />
-                    </Col>
-                </Row>
-            </div>
-        );
-    }
+		return (
+			<div className='container btw-message-list'>
+				<Typography>Message list</Typography>
+				<Row className='chat-content'>
+					<Col md={4}>
+						<div className='chats'>
+							{ chats.map((chat, i) => {
+								return (
+									<Row key={i}
+									     className={classnames('chat', { 'selected': chat._id === selectedChatId })}
+									     onClick={() => this.props.actions.selectChat(chat._id) }>
+										<Col md={8}>
+											<div>{ this.formatMessage(chat.message) }</div>
+										</Col>
+										<Col md={4}>
+											<div>
+												<Moment format="MM-DD HH:mm">
+													{ chat.date }
+												</Moment>
+											</div>
+										</Col>
+									</Row>
+								)
+							})}
+							{ chats.length === 0 &&
+							<div className='msg-text'>
+								<Typography>No conversations</Typography>
+							</div> }
+						</div>
+					</Col>
+					<Col md={8}>
+						<ChatBody />
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => {
-    return {
-        chats: state.chats
-    }
+	return {
+		chats: state.chats
+	}
 };
 
 const mapDispatchToProps = (dispatch) => ({
-    actions: bindActionCreators({ loadChats, selectChat }, dispatch)
+	actions: bindActionCreators({ loadChats, selectChat }, dispatch)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(MessageList);
